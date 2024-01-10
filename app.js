fd1e025d8175437036ad8212f4985cdb4b286927
Ebenezer Sakyi <94199812+EbenezerSakyi621@users.noreply.github.com>
2024-01-10 12:07:45 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+// You can add interactive features or animations here
+console.log('Welcome to the artist\'s website!');
+
+
---
+++ scripts.js
@@ -0,0 +1,3 @@
+// You can add interactive features or animations here
+console.log('Welcome to the artist\'s website!');
+
